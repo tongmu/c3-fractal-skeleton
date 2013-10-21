44c62bb59c06d8872240cd8fd8c8bcbbde3cc7f7
Tong Mu <waterfallperson@gmail.com>
2013-10-21 15:04:14 -0700

    b

---
+++ src/Mandlebrot/Mandlebrot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mandlebrot
+//
+//  Created by Tong Mu on 10/21/13.
+//  Copyright (c) 2013 Tong Mu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
