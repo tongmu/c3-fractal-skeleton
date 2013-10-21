7efd01e07d4134dff22a2d8fe396df94e938d33f
Tong Mu <waterfallperson@gmail.com>
2013-10-20 21:51:12 -0700

    Mandelbrot.cpp

--- src/Mandelbrot.cpp
+++ src/Mandelbrot.cpp
@@ -1,4 +1,5 @@
 #include "Mandelbrot.h"
+#include <cmath>
 
 void Mandelbrot::gen_fractal()
 {
@@ -6,6 +7,32 @@ void Mandelbrot::gen_fractal()
 	// Imaginary (-1, 1)
     
     // Iterate over each pixel and determine the corresponding complex coordinate (or several complex coordinates if you're anti-aliasing)
+    int height=get_height();
+    int width=get_width();
+    int i, npixels=height*width;
+    for(i=0; i<npixels;i++)
+    {
+        int x=i%height;
+        int y=i/height;
+        double c_r=(((double)x/width)*3.0-2.25);
+        double c_i=(((double)y)/height*3.0-1.5);
+        double z_r=0;
+        double z_i=0;
+        double z_temp;
+        int n=0;
+        while(pow(z_r,2)+pow(z_i,2)<4&&n<1000)
+        {
+            z_temp=pow(z_r,2)-pow(z_i,2)+c_r;
+            z_i=z_r*z_i*2+c_i;
+            z_r=z_temp;
+            n++;
+        }
+        m_bitmap[x*m_height*4+y*4+0]=pow((n/1000),.6)*255;
+        m_bitmap[x*m_height*4+y*4+1]=pow((n/1000),.5)*255;
+        m_bitmap[x*m_height*4+y*4+1]=pow((n/1000),.4)*255;
+        m_bitmap[x*m_height*4+y*4+3]=255;
+        
+    }
     
 
         // For each pixel, compute the corresponding complex coordinate C (or multiple sub-coordinates, for AA)   
